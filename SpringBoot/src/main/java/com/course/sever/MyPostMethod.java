41b5348a82eb9836a44383831813dbb4e0b551e6
John Doe <john@sample.com>
2019-08-07 13:37:56 +0800

    上传

--- SpringBoot/src/main/java/com/course/sever/MyPostMethod.java
+++ SpringBoot/src/main/java/com/course/sever/MyPostMethod.java
@@ -1,12 +1,33 @@
 package com.course.sever;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@Api(value = "/",description = "这是我全部的post方法" )
+@Api(value = "/",description = "这是我全部的post请求")
 @RequestMapping("/v1")
 public class MyPostMethod {
 
+    //这个变量是用来装我们cookies信息的
+    private static Cookie cookie;
+
+    //用户登陆成功获取到cookies，然后再访问其他接口获取到列表
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ApiOperation(value = "登陆接口，成功后获取cookies信息",httpMethod = "POST")
+    public String login(HttpServletResponse response,
+                        @RequestParam(value = "userName",required = true) String userName,
+                        @RequestParam(value = "password",required = true)  String password){
+        if(userName.equals("zhangsan") && password.equals("123456")){
+            cookie = new Cookie("login","true");
+            response.addCookie(cookie);
+            return "恭喜你登陆成功了!";
+        }
+        return "用户名或者是密码错误！";
+    }
 }
+
